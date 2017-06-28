b289a698a2ce44ded813f6bf779438fa43d2a599
RangiBecerra <rangibecerra@gmail.com>
2017-06-28 12:39:42 -0400

    archivos base

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+//alert("existe");
+$ (document).ready (function() {
+ 	// codigo
+ 	$("button").click(function(e){
+ 		e.preventDefault();
+ 		var name = $("#nombre").val();
+ 		var message = $("#mensaje").val();
+ 		if(name == "" && message == "") {
+ 			alert("Debes escribir tu nombre y el mensaje");
+ 		} else {
+ 			$("#comentarios").append("<div><span class='center-align')" + name + "</span><p class='center-align'>" + message + "</p></div");
+ 		}
+ 	});
+});
