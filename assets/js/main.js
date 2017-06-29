4597f036f0e1c168ee9998a1ceb86719cdbe6dc2
RangiBecerra <rangibecerra@gmail.com>
2017-06-29 16:09:00 -0400

    validacion de cambio vacio y saque el estilo de alineacion de boton porque me producia error al remover tareas

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,19 +6,24 @@ $(document).ready(function() {
  		if(x.keyCode == 13) {
        		// value input
        		var value = $("#first_name").val();
-       		$("#tareas").append("<div class='nuevo'><input type='checkbox' id='test-" + contador + "' /><label for='test-" + contador + "'>" + value + "</label> <div class='right-align'> <a id='remove' class='waves-effect waves-light btn deep-purple'>remove</a></div></div>");
-       		// tareas borradas
-                  $("a").click(function(){
+                  // verificar si value esta vacio sino ejecuta la tarea
+                  if (value == "") {
+                        alert("Debes escribir una tarea");
+                  } else {
+       		      $("#tareas").append("<div class='nuevo'><input type='checkbox' id='test-" + contador + "' /><label for='test-" + contador + "'>" + value + "</label><a id='remove' class='waves-effect waves-light btn deep-purple'>remove</a></div>");
+       		      // tareas borradas
+                        $("a").click(function(){
                         $(this).parent().remove();
-                  });
-                  // limpiar input
-                  $("#first_name").val("");
-                  // tareas completas
-                  $("#test-" + contador).click(function(){
+                        });
+                        // limpiar input
+                        $("#first_name").val("");
+                        // tareas completas
+                        $("#test-" + contador).click(function(){
                         $("#completadas").append($(this).parent());
-                  });
-                  // marcar contador
-                  contador++;
-       	}
+                        });
+                        // marcar contador
+                        contador++;
+                  }
+       	} 
       });
 });
