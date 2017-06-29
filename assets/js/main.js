fa950328b614877257c4ffd49a2a93f93834bfd7
RangiBecerra <rangibecerra@gmail.com>
2017-06-29 00:52:32 -0400

    orden de codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,17 @@
 //alert("existe");
-$ (document).ready (function() {
- 	// codigo
- 	$("button").click(function(e){
- 		e.preventDefault();
- 		var name = $("#nombre").val();
- 		var message = $("#mensaje").val();
- 		if(name == "" && message == "") {
- 			alert("Debes escribir tu nombre y el mensaje");
- 		} else {
- 			$("#comentarios").append("<div><span class='center-align')" + name + "</span><p class='center-align'>" + message + "</p></div");
- 		}
- 	});
+$(document).ready(function() {
+ 	// codigo (13 codigo ASCII)
+ 	$("body").keyup(function(x) {
+ 		if(x.keyCode == 13) {
+ 			//alert("trabaja");
+       		// value input
+       		var value = $("#first_name").val();
+       		$("#tareas").append("<p><input class='with-gap' name='group1' type='radio' id='test1' /><label for='test1'>" + value + "</label> <a class='waves-effect waves-light btn'>remove</a></p>");
+       		$("#first_name").val("");
+       	}
+       	// tareas completas
+       	$("#test1").keyup(function(){
+       		alert("casi completadas");
+       	});
+    });
 });
