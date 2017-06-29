d68f8d7e54d6e3e76d69dc1809d883068f1eda2c
RangiBecerra <rangibecerra@gmail.com>
2017-06-29 10:10:17 -0400

    funcionalidad jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,25 @@
 //alert("existe");
 $(document).ready(function() {
+      var contador = 0;
  	// codigo (13 codigo ASCII)
  	$("body").keyup(function(x) {
  		if(x.keyCode == 13) {
- 			//alert("trabaja");
        		// value input
        		var value = $("#first_name").val();
-       		$("#tareas").append("<p><input class='with-gap' name='group1' type='radio' id='test1' /><label for='test1'>" + value + "</label> <a class='waves-effect waves-light btn'>remove</a></p>");
-       		$("#first_name").val("");
+       		$("#tareas").append("<div class='nuevo'><input type='checkbox' id='test-" + contador + "' /><label for='test-" + contador + "'>" + value + "</label> <a id='remove' class='waves-effect waves-light btn'>remove</a></div>");
+       		// tareas borradas
+                  $("a").click(function(){
+                        $(this).parent().remove();
+                  });
+                  // limpiar input
+                  $("#first_name").val("");
+                  // tareas completas
+                  $("#test-" + contador).click(function(){
+                        console.log("aqui voy");
+                        $("#completadas").append($(this).parent());
+                  });
+                  // marcar contador
+                  contador++;
        	}
-       	// tareas completas
-       	$("#test1").keyup(function(){
-       		alert("casi completadas");
-       	});
-    });
+      });
 });
